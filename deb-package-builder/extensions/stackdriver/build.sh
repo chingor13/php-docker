d9391ca2c17847893dacebb80d3a63633f0fb30f
Jeff Ching <chingor@google.com>
2017-04-06 15:37:21 -0700

    can build extension as debian package

---
+++ deb-package-builder/extensions/stackdriver/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+source ${DEB_BUILDER_DIR}/extensions/functions.sh
+
+echo "Building stackdriver for gcp-php${SHORT_VERSION}"
+
+PNAME="gcp-php${SHORT_VERSION}-stackdriver"
+EXT_VERSION="0.1.0"
+PACKAGE_VERSION="${EXT_VERSION}-${PHP_VERSION}"
+PACKAGE_FULL_VERSION="${EXT_VERSION}-${FULL_VERSION}"
+PACKAGE_DIR=${PNAME}-${PACKAGE_VERSION}
+cp -R ${DEB_BUILDER_DIR}/extensions/stackdriver/src/stackdriver/ext/stackdriver ${PACKAGE_DIR}
+cp -R ${DEB_BUILDER_DIR}/extensions/stackdriver/debian ${PACKAGE_DIR}/
+
+pwd
+ls -al
+build_package stackdriver
