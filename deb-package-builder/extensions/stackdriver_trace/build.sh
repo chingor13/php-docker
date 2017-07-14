4f5fe34214741d73bf8e471b9b3b833c08f36b5d
Jeff Ching <chingor@google.com>
2017-07-14 13:35:28 -0700

    Can build stackdriver_trace extension from GitHub

--- deb-package-builder/extensions/stackdriver_trace/build.sh
+++ deb-package-builder/extensions/stackdriver_trace/build.sh
@@ -4,18 +4,12 @@ set -ex
 
 source ${DEB_BUILDER_DIR}/extensions/functions.sh
 
-echo "Building stackdriver for gcp-php${SHORT_VERSION}"
+echo "Building stackdriver trace for gcp-php${SHORT_VERSION}"
 
 PNAME="gcp-php${SHORT_VERSION}-stackdriver-trace"
-# EXT_VERSION="0.1.0"
-# PACKAGE_VERSION="${EXT_VERSION}-${PHP_VERSION}"
-# PACKAGE_FULL_VERSION="${EXT_VERSION}-${FULL_VERSION}"
-# PACKAGE_DIR=${PNAME}-${PACKAGE_VERSION}
+EXT_VERSION="0.1.0"
 
-BRANCH="trace-extension"
-
-download_from_tarball https://github.com/chingor13/google-cloud-php/archive/${BRANCH}.tar.gz 0.1.0
-PACKAGE_DIR=${PACKAGE_DIR}/src/Trace/ext
+download_from_tarball https://github.com/GoogleCloudPlatform/stackdriver-trace-php-extension/archive/master.tar.gz ${EXT_VERSION}
 
 pwd
 ls -al
